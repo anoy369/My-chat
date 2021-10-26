5249eb36fee4350e8623babf692c036a1ecadbd8
anoy369 <adanoy369@gmail.com>
2021-10-26 06:52:47 +0600

    add logout to navbar component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default function navbar() {
+export default function Navbar({ user, setUser }) {
+  let history = useHistory();
+
+  const logout = () => {
+    localStorage.setItem("user", null);
+    setUser(null);
+    history.push("/");
+  };
   return (
     <div>
-      <p>Navbar</p>
+      {user ? (
+        <div>
+          <div>welcome, {user.email}</div>
+          <div onClick={logout}>Logout</div>
+        </div>
+      ) : (
+        <div>
+          <p>Login to use chat</p>
+        </div>
+      )}
     </div>
   );
 }
